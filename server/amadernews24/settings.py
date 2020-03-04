c52bfef60818c69f5496eea8c5ba6e6136b2f69e
sagor09 <mbrsagor@gmail.com>
2020-03-05 01:00:35 +0600

    Use mysql database

--- server/amadernews24/settings.py
+++ server/amadernews24/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'amadernews24.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newspaper',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
