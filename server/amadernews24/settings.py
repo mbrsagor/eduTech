5cf97bd29a9c41cc3ccb3d34e76933451f0c271f
Sagor <mbrsagor@gmail.com>
2020-12-25 23:42:52 +0600

    SMTP mail sending

--- server/amadernews24/settings.py
+++ server/amadernews24/settings.py
@@ -139,3 +139,13 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000'
 ]
+
+
+# SMTP mail settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'newspaper@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
