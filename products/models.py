da804bb24c38f4317a3f106bb97c35ee15107e77
mbrsagor <mbrsagor@gmail.com>
2021-09-04 00:42:59 +0600

    New: Listing API not done yat

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.timezone import now
+from django.db.models import JSONField
 
 
 class Product(models.Model):
@@ -12,3 +14,36 @@ class Product(models.Model):
 
 class User(models.Model):
     pass
+
+
+class Listing(models.Model):
+    class SaleType(models.TextChoices):
+        FOR_SALE = 'For Sale'
+        FOR_RENT = 'For Rent'
+
+    class HomeType(models.TextChoices):
+        HOUSE = 'House'
+        CONDO = 'Condo'
+        TOWNHOUSE = 'Townhouse'
+
+    realtor = models.EmailField(max_length=120)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    address = models.TextField()
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zip = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathroom = models.DecimalField(max_digits=2, decimal_places=1)
+    sale_type = models.CharField(max_length=10, choices=SaleType.choices, default=SaleType.FOR_RENT)
+    home_type = models.CharField(max_length=10, choices=HomeType.choices, default=HomeType.HOUSE)
+    photo = models.ImageField(upload_to='listing', blank=True, null=True)
+    gallery = JSONField(blank=True, null=True, default=None)
+    is_publish = models.BooleanField(default=False)
+    is_available = models.BooleanField(default=True)
+    created_date = models.DateTimeField(now)
+
+    def __str__(self):
+        return f"{self.title} and Price: {self.price}"
--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ import random
 from rest_framework import viewsets, views, status
 from rest_framework.response import Response
 
-from .models import Product, User
+from .models import Product, User, Listing
 from .producer import publish
 from .serializers import ProductSerializer
 
@@ -46,3 +46,66 @@ class UserAPIView(views.APIView):
         return Response({
             'id': user.id
         })
+
+
+class ManageListingView(views.APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        try:
+            user = request.user
+            if not user.is_superuser:
+                return Response(
+                    {'error': 'Use does not have permissions for creating this listing data'},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+            data = request.data
+            realtor = data['realtor']
+            title = data['title']
+            slug = data['slug']
+            address = data['address']
+            city = data['city']
+            state = data['state']
+            zip = data['zip']
+            description = data['description']
+            price = data['price']
+            try:
+                price = int(price)
+            except:
+                return Response(
+                    {'error': 'Price must be integer'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            bedrooms = data['bedrooms']
+            try:
+                bedrooms = int(bedrooms)
+            except:
+                return Response(
+                    {'error': 'bedrooms must be integer'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            bathroom = data['bathroom']
+            try:
+                bathroom = float(bathroom)
+            except:
+                return Response(
+                    {'error': 'bathroom must be float'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            if bedrooms <= 0 or bathroom >= 0:
+                bedrooms = 1.0
+
+            sale_type = data['sale_type']
+            home_type = data['home_type']
+            photo = data['photo']
+            gallery = data['gallery']
+            is_publish = data['is_publish']
+            is_available = data['is_available']
+            created_date = data['created_date']
+
+        except:
+            return Response(
+                {'error': 'something went to wrong when creating listing'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
