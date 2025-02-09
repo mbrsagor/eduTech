72eb5ce96b67646925fd4fdaa2e65103f16fa448
mbrsagor <mbrsagor@gmail.com>
2025-02-10 00:26:59 +0600

    New: Create new blog  post done

--- blog/forms.py
+++ blog/forms.py
@@ -14,3 +14,21 @@ class CategoryForm(forms.ModelForm):
             'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             'icon': forms.FileInput(attrs={'class': 'form-control', 'id': 'icon', 'accept': 'image/*'}),
         }
+
+
+class PostForm(forms.ModelForm):
+    """
+    Name: Post form for creating and updating posts
+    """
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'category', 'thumbnail', 'is_publish')
+        read_only_fields = ('author',)
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'id': 'title', 'placeholder': 'Enter post title'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'id': 'content', 'placeholder': 'Enter post content'}),
+            'category': forms.Select(attrs={'class': 'form-control', 'id': 'category'}),
+            'thumbnail': forms.FileInput(attrs={'class': 'form-control', 'id': 'thumbnail', 'accept': 'image/*'}),
+            'is_publish': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+    
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import dashboard_view, category_view
+from blog.views import dashboard_view, category_view, post_view
 
 
 urlpatterns = [
@@ -10,4 +10,9 @@ urlpatterns = [
     path('create-category', category_view.CreateCategoryView.as_view(), name='create_category'),
     path('category-update/<pk>', category_view.CategoryUpdateView.as_view(), name='update_category'),
     path('category-delete/<pk>', category_view.CategoryDeleteView.as_view(), name='delete_category'),
+    # Posts
+    path('posts', post_view.PostListView.as_view(), name='posts'),
+    path('create-post', post_view.CreatePostView.as_view(), name='create_post'),
+    # path('post-update/<pk>', post_view.PostUpdateView.as_view(), name='update_post'),
+    # path('post-delete/<pk>', post_view.PostDeleteView.as_view(), name='delete_post'),
 ]
