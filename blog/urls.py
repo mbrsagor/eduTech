c437c7d2107d14d71d298f0c4158e915d4ca489f
mbrsagor <mbrsagor@gmail.com>
2025-02-10 00:06:14 +0600

    New: Category CRUD done

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # Dashboard
     path('dashboard/', dashboard_view.DashboardView.as_view(), name='dashboard'),
     # Categories
+    path('categories', category_view.CategoryListView.as_view(), name='categories'),
     path('create-category', category_view.CreateCategoryView.as_view(), name='create_category'),
+    path('category-update/<pk>', category_view.CategoryUpdateView.as_view(), name='update_category'),
+    path('category-delete/<pk>', category_view.CategoryDeleteView.as_view(), name='delete_category'),
 ]
--- blog/views/category_view.py
+++ blog/views/category_view.py
@@ -19,3 +19,40 @@ class CreateCategoryView(SuccessMessageMixin, generic.CreateView):
     success_url = '/blog/create-category'
     success_message = 'Category created successfully.'
     template_name = 'category/create_category.html'
+
+
+
+@method_decorator(login_required(login_url='/user/signin/'), name='dispatch')
+class CategoryListView(generic.ListView):
+    """
+    Category listing using CMS
+    """
+    model = Category
+    template_name = 'category/category_list.html'
+    context_object_name = 'categories'
+    paginate_by = 8
+    ordering = ['-created_at']
+
+
+@method_decorator(login_required(login_url='/user/signin/'), name='dispatch')
+class CategoryUpdateView(SuccessMessageMixin, generic.UpdateView):
+    """
+    Category updating using CMS
+    """
+
+    model = Category
+    form_class = forms.CategoryForm
+    success_url = '/blog/categories'
+    success_message = 'Category created successfully.'
+    template_name = 'category/create_category.html'
+
+
+@method_decorator(login_required(login_url='/user/signin/'), name='dispatch')
+class CategoryDeleteView(SuccessMessageMixin, generic.DeleteView):
+    """
+    Category deleting using CMS
+    """
+    model = Category
+    success_url = '/blog/categories'
+    success_message = 'The category has been deleted.'
+    template_name = 'common/delete_confirm.html'
