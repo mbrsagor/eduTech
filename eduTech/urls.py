d421a298df5264ad0d073338d5d579bf738fd3d7
mbrsagor <mbrsagor@gmail.com>
2025-02-19 23:16:33 +0600

    Refactor: Message and delete confirm template back

--- eduTech/urls.py
+++ eduTech/urls.py
@@ -24,8 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
-    path('', include('dashboard.urls')),
+    path('', include('user.urls')),
 ]
 
 if settings.DEBUG:
