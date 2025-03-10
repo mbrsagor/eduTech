3c41e3321655c12e6e4cf7b467960c9fbfb2d861
mbrsagor <mbrsagor@gmail.com>
2025-03-10 12:30:31 +0600

    New: Search result show in deaitls page

--- classify/urls.py
+++ classify/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import classify_question, classify_question_history
+from .views import classify_question, classify_question_history, search_results
 
 urlpatterns = [
     path("", classify_question, name="question_classification"),
-    path("history/", classify_question_history, name="history")
+    path("history/", classify_question_history, name="history"),
+    path("search_results/", search_results, name="search_results"),
 ]
--- classify/views.py
+++ classify/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .forms import QuestionForm
 from .models import History
@@ -6,17 +7,25 @@ from .ml_model.predict import predict_question_level
 
 
 def classify_question(request):
-    prediction = None
     form = QuestionForm(request.POST or None)
     if request.method == "POST" and form.is_valid():
         question = form.cleaned_data["question"]
         prediction = predict_question_level(question)
         # Save history
         History.objects.create(title=question, classification=prediction, user_id=request.user.id)
-    return render(request, "classify.html", {"form": form, "prediction": prediction})
+        # Redirect to search results page with prediction as a query parameter
+        return redirect(f"{reverse('search_results')}?query={question}&prediction={prediction}")
+    return render(request, "classify.html", {"form": form})
 
 
 def classify_question_history(request):
     history = History.objects.filter(user_id=request.user.id)
     context = {"history": history}
     return render(request, "history.html", context)
+
+
+def search_results(request):
+    query = request.GET.get("query", "")
+    prediction = request.GET.get("prediction", "")
+    print(f"prediction: {prediction}")
+    return render(request, "search_results.html", {"query": query, "prediction": prediction})
